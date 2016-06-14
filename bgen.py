f9770ff369a7facd873becd0ed28fa4c330e9b19
encima <encima@gmail.com>
2016-06-14 14:17:22 +0100

    modified to not store grid within the instance, makes it easier to load in grids and check the answers

--- bgen.py
+++ bgen.py
@@ -8,26 +8,15 @@ class Bakuro_Gen():
 		self.bits = bits
 		#calculate the max number that can be represented by the bits
 		self.max_num = (2**self.bits)-1
-		self.generate_grid()
-
-	def generate_grid(self):
-		self.grid = [[random.randrange(0, self.max_num) for _ in range(self.grid_size - 1)] for _ in range(self.grid_size - 1)]
-		for row in self.grid:
-			for index,val in enumerate(row):
-				if(val == 0):
-					row[index] = None
-				else:
-					row[index] = {'dec':val, 'bin':bin(val)}
-		
-
-	def solve_grid(self):
+	
+	def solve_grid(self, grid):
 		#Columns
 		col_totals = []
-		for row_ind, row in enumerate(zip(*self.grid)):
+		for row_ind, row in enumerate(zip(*grid)):
 			col_total = 0
 			for col_ind, val in enumerate(row):
 				if val is None:
-					self.grid[col_ind][row_ind] = {'col_total':col_total}
+					grid[col_ind][row_ind] = {'col_total':col_total}
 					col_total = 0
 				elif col_ind == len(row) - 1:
 					col_total += val['dec']
@@ -36,7 +25,7 @@ class Bakuro_Gen():
 				else:
 					col_total += val['dec']
 		#Rows
-		for index, row in enumerate(self.grid):
+		for index, row in enumerate(grid):
 			row_total = 0
 			for ind, val in enumerate(row):
 				if 'col_total' in val:
@@ -48,22 +37,31 @@ class Bakuro_Gen():
 					break
 				else:
 					row_total += val['dec']
-		self.grid.append(col_totals)
+		grid.append(col_totals)
+		return grid
 
+	def generate_grid(self):
+		grid = [[random.randrange(0, self.max_num) for _ in range(self.grid_size - 1)] for _ in range(self.grid_size - 1)]
+		for row in grid:
+			for index,val in enumerate(row):
+				if(val == 0):
+					row[index] = None
+				else:
+					row[index] = {'dec':val, 'bin':bin(val)}
+		return self.solve_grid(grid)
 
-
-	def print_grid(self, binary = False, solved = True):
-		for row_index, row in enumerate(self.grid):
+	def print_grid(self, grid, binary = False, solved = True):
+		for row_index, row in enumerate(grid):
 			unsolved = ''
 			for col_index, value in enumerate(row):
-				val = self.grid[row_index][col_index]
+				val = grid[row_index][col_index]
 				if 'bin' in val:
 					if binary:
 						unsolved += str(val['bin']) + ' . '
 					else:
 						unsolved += str(val['dec']) + ' . '
-				elif ('row_total' in self.grid[row_index][col_index] or 'col_total' in self.grid[row_index][col_index]) and solved:
-					unsolved += str((self.grid[row_index][col_index])) + ' . '
+				elif ('row_total' in val or 'col_total' in val) and solved:
+					unsolved += str(val) + ' . '
 				else:
 					unsolved += ('[  ] . ')
 			print(unsolved)
@@ -75,8 +73,9 @@ parser.add_argument('--bits', default=4, help='size of numbers to generate', typ
 parser.add_argument('--sol', default=True, help='Print the solved grid', type=bool)
 args = parser.parse_args()
 b = Bakuro_Gen(args.grid_size, args.bits)
-b.solve_grid()
-b.print_grid(binary = False, solved = True)
-b.print_grid(binary = False, solved = False)
+grid = b.generate_grid()
+b.print_grid(grid, binary = False, solved = False)
+print('---------')
+b.print_grid(grid, binary = False, solved = True)
 #TODO solve grids passed to program in txt file
 #TODO prettify grid printing and add zero padding
